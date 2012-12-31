5f49952d88c61b8c30809619b6b639e083634f69
John Grogg <johng@jakks.net>
2012-12-31 14:19:12 -0800

    add redistogo connection

--- helpers.js
+++ helpers.js
@@ -2,7 +2,16 @@ var redis = require('redis')
   , moment = require('moment')
   , crypto = require("crypto")
   , assert = require("assert");
-exports.client = redis.createClient();
+// Export modules
+if (process.env.REDISTOGO_URL) {
+  // redistogo connection
+  var rtg   = require("url").parse(process.env.REDISTOGO_URL);
+  var client = redis.createClient(rtg.port, rtg.hostname);
+  client.auth(rtg.auth.split(":")[1]);
+  exports.client = client;
+} else {
+  exports.client = redis.createClient();
+}
 exports.moment = moment;
 exports.crypto = crypto;
 exports.assert = assert;
